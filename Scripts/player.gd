88438c08a27b3943ae1795a64d1836bb45bf0d5a
ROJS <mskim829@naver.com>
2025-10-07 22:02:42 +0900

    Implementing Player Movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,4 +11,6 @@ func _physics_process(delta: float) -> void:
 	#Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	#Get the input 
+	#Get the input direction and handle the movement/deceleration.
+	#As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
