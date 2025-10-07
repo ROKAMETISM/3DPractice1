5aa3addce936bcd5c39d3dac558b2cb6f0cfa58b
ROJS <mskim829@naver.com>
2025-10-07 22:00:59 +0900

    Implementing Player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,1 +1,14 @@
 extends CharacterBody3D
+
+const SPEED := 5.0
+const JUMP_VELOCITY := 4.5
+#Get the gravity from the project settings to be synced with Rigidbody nodes.
+var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+func _physics_process(delta: float) -> void:
+	#Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	#Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	#Get the input 
