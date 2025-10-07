c9196db3da963d03e3aebd0f3ee5910fa11a7299
ROJS <mskim829@naver.com>
2025-10-07 22:41:55 +0900

    Add box to world

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -23,13 +23,18 @@ func _physics_process(delta: float) -> void:
 	#Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	#Handle sprint.
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
 	#Get the input direction and handle the movement/deceleration.
 	#As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (headpivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction : 
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
