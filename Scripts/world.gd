e571bd5b9169f1fc68cf73793b571ee4d35db280
ROJS <mskim829@naver.com>
2025-10-10 23:51:38 +0900

    create UI node

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,4 +1,6 @@
 extends Node3D
+@onready var player := %Player
+@onready var debugtext := %DebugText
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
