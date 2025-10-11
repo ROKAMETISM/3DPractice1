52a688febb60ce0c68448f2e27ea5359f79f5a05
ROJS <mskim829@naver.com>
2025-10-11 22:15:33 +0900

    Create Pistol

--- Scripts/weapon_manager.gd
+++ Scripts/weapon_manager.gd
@@ -1,22 +1,17 @@
 extends Node3D
-const RAY := preload("uid://be2ixbaa5oacl")
-const FIRE_RATE := 0.2
-const RAY_LENGTH := 30.0
-var fire_timer := 0.0
+const PISTOLPRELOAD := preload("uid://kft8ejopthmq")
 var player : CharacterBody3D
+var weapons : Array[Node3D]
+var current_weapon_index := 0
+func _ready() -> void:
+	var pistol := PISTOLPRELOAD.instantiate()
+	add_child(pistol)
+	weapons.append(pistol)
+	pistol.player = player
 func fire() -> void:
 	if not player:
 		return
-	if fire_timer <= 0.0:
-		fire_timer = FIRE_RATE
-		var new_ray = RAY.instantiate()
-		get_tree().current_scene.add_child(new_ray)
-		var points : Array[Vector3]
-		points.append(player.global_position)
-		points.append(player.global_position + player.pointing_vector * RAY_LENGTH)
-		new_ray.update_points(points)
-		
-func _physics_process(delta: float) -> void:
-	if fire_timer > 0.0:
-		fire_timer = maxf(fire_timer - delta, 0.0)
+	if not weapons[current_weapon_index]:
+		return
+	weapons[current_weapon_index].fire()
 	
