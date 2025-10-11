197a8e8a7a9fcf737e18b5289e7b99866607f8ce
ROJS <mskim829@naver.com>
2025-10-11 23:02:23 +0900

    Update shot_hit_particle.gd

--- Scripts/shot_hit_particle.gd
+++ Scripts/shot_hit_particle.gd
@@ -1,5 +1,6 @@
 extends MeshInstance3D
 const SPEED := 2.0
+const GRAVITY := 9.8
 var lifetime := 0.5
 var velocity := Vector3.ZERO
 func _physics_process(delta: float) -> void:
@@ -7,5 +8,6 @@ func _physics_process(delta: float) -> void:
 	if lifetime < 0.0:
 		queue_free()
 	position += velocity * delta
+	velocity.y -= GRAVITY * delta
 func set_direction(direction : Vector3) -> void:
 	velocity = direction * SPEED
