96bb4b17ac103f8e1f16df6bf0ce705f8277f19e
ROJS <mskim829@naver.com>
2025-10-11 23:21:34 +0900

    Setup Enemy Hurtbox

--- Scripts/enemy_test.gd
+++ Scripts/enemy_test.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED := 5.0
+const JUMP_VELOCITY := 4.5
+const MAX_HP := 10.0
+var current_hp := MAX_HP
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
