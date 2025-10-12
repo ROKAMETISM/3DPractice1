36365020a5a1dad27580952b21020e5dd004953f
ROJS <mskim829@naver.com>
2025-10-12 14:00:56 +0900

    Fix Crosshair : Issue was that FOV was in degree and Weapon Spread angle was in Radian

--- Scripts/crosshair.gd
+++ Scripts/crosshair.gd
@@ -6,8 +6,8 @@ func _draw() -> void:
 		return
 	var viewport_dimension : Vector2 = get_viewport_rect().size
 	var spread_radius : float = min(viewport_dimension.x, viewport_dimension.y)
-	spread_radius = spread_radius * (tan(_weapon_spread) / tan(FOV / 2))
-	print("WeaponSpread:%.2f"%_weapon_spread)
+	spread_radius = spread_radius * (tan(_weapon_spread) / tan(deg_to_rad(FOV / 2)))
+	print("WeaponSpread:%.2f"%rad_to_deg(_weapon_spread))
 	print("FOV / 2 :%.2f"%(FOV/2))
 	print("Spread Radius:%.2f"%spread_radius)
 	draw_circle(size/2+Vector2(-0.5,-0.5), spread_radius, Color.WHITE, false, 1.0, true)
