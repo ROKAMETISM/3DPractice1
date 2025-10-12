3879fbd36cb2c528c0c2f3069ec2bf5876df0f9a
ROJS <mskim829@naver.com>
2025-10-12 14:52:09 +0900

    Implement Simple Enemy AI

--- Scripts/crosshair.gd
+++ Scripts/crosshair.gd
@@ -1,4 +1,5 @@
 extends TextureRect
+const CIRCLE_COLOR := Color(0.0, 1.0, 1.0, 0.7)
 var _weapon_spread := -1.0
 var _fov := 75.0
 
@@ -8,10 +9,7 @@ func _draw() -> void:
 	var viewport_dimension : Vector2 = get_viewport_rect().size
 	var spread_radius : float = min(viewport_dimension.x, viewport_dimension.y)
 	spread_radius = spread_radius / 2 * (tan(_weapon_spread) / tan(deg_to_rad(_fov / 2)))
-	print("WeaponSpread:%.2f"%rad_to_deg(_weapon_spread))
-	print("FOV / 2 :%.2f"%(_fov/2))
-	print("Spread Radius:%.2f"%spread_radius)
-	draw_circle(size/2+Vector2(-0.5,-0.5), spread_radius, Color.WHITE, false, -1.0, true)
+	draw_circle(size/2+Vector2(-0.5,-0.5), spread_radius, CIRCLE_COLOR, false, -1.0, false)
 func set_weapon_spread(weapon_spread:float)->void:
 	_weapon_spread=weapon_spread
 	queue_redraw()
--- Scripts/enemy_test.gd
+++ Scripts/enemy_test.gd
@@ -1,12 +1,17 @@
 extends CharacterBody3D
-const SPEED := 5.0
+const SPEED := 2.0
 const JUMP_VELOCITY := 4.5
 const MAX_HP := 10.0
 const HIT_ANIMATION_DURATION := 0.3
 const GRAVITY := 10.0
+const PAUSE_TIME := 8.0
+const PAUSE_TIME_SPREAD := 2.0
+const WANDER_RADIUS := 16.0
+const WANDER_DESTINATION_REACH_THRESHOLD := 5.0
 var current_hp := MAX_HP
 var hit_animation_timer := 0.0
-var ai_state := state.PAUSE
+var current_state := state.PAUSE
+var wander_destination := Vector3.ZERO
 @onready var sprite = %Sprite
 enum state {
 	WANDER,
@@ -14,8 +19,10 @@ enum state {
 }
 func _ready() -> void:
 	sprite.animation = "default"
+	_start_wander()
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	iterate_ai()
 	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
 	move_and_slide()
@@ -24,7 +31,15 @@ func _physics_process(delta: float) -> void:
 		hit_animation_timer = 0.0
 		sprite.animation = "default"
 func iterate_ai()->void:
-	pass
+	if current_state == state.PAUSE:
+		return
+	if current_state == state.WANDER:
+		look_at(wander_destination, Vector3.UP)
+		velocity = -transform.basis.z*SPEED
+		if (wander_destination-global_position).length() < WANDER_DESTINATION_REACH_THRESHOLD:
+			current_state = state.PAUSE
+			print("wander destination reached")
+			get_tree().create_timer(PAUSE_TIME+randf_range(-1.0, 1.0)*PAUSE_TIME_SPREAD).timeout.connect(_pause_timer_timeout)
 func take_damage(damage : float) -> void:
 	current_hp -= damage
 	if current_hp <= 0.0:
@@ -34,3 +49,12 @@ func take_damage(damage : float) -> void:
 	hit_animation_timer = HIT_ANIMATION_DURATION
 func _die() -> void:
 	queue_free()
+func _get_next_wander_position()->Vector3:
+	print("get new wander destination")
+	var dist := sqrt(randf())*WANDER_RADIUS
+	return Vector3(dist, 0.0, 0.0).rotated(Vector3.UP, randf_range(0.0, 2*PI))
+func _pause_timer_timeout()->void:
+	_start_wander()
+func _start_wander()->void:
+	current_state = state.WANDER
+	wander_destination = _get_next_wander_position()
