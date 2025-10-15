8b6f378caa8a9e7d3580490f1f62f6d433b82e88
ROJS <mskim829@naver.com>
2025-10-15 23:28:02 +0900

    Implement Console command

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,4 +1,5 @@
 extends Node3D
+const ENEMY_TEST := preload("uid://du3tuubqmh3i8")
 @onready var player := %Player
 @onready var hud := %HUD
 func _ready() -> void:
@@ -8,8 +9,9 @@ func _ready() -> void:
 	player.weapon_manager.weapon_switched.connect(hud.set_current_weapon)
 	player.player_fov_updated.connect(hud.crosshair.set_fov)
 	player.signal_fsm_state_updated.connect(hud.set_player_fsm_states)
-	print("debug signal connected")
 	player.weapon_manager.switch_weapon(0)
+	Console.add_command("SpawnEnemyTest", spawn_enemy_test, 1)
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
@@ -17,4 +19,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("restart_game"):
 		get_tree().reload_current_scene()
 		return
+
+func spawn_enemy_test(param : String)->void:
+	Console.print_line("Spawning at : %s" % str(param))
+	var components = param.split(",", false)
+	if components.size() != 3:
+		return
+	var spawn_location : Vector3 = Vector3(float(components[0]), float(components[1]), float(components[2]))
+	var new_enemy := ENEMY_TEST.instantiate()
+	new_enemy.global_position = spawn_location
+	add_child(new_enemy)
 	
