5e6d7b3715b1bc40e6acf41a73a07b9dc054a5ca
ROJS <mskim829@naver.com>
2025-10-15 23:36:41 +0900

    Update EnemyAI

--- Scripts/StateMachine/Controllers/enemy_test_move_controller.gd
+++ Scripts/StateMachine/Controllers/enemy_test_move_controller.gd
@@ -3,8 +3,11 @@ extends MoveController
 const WANDER_RANGE := 16.0
 const JUMP_RATE := 3.5
 const JUMP_RATE_SPREAD := 1.0
+const SPRINT_RATE := 2.0
 var jump_timer := 2.0
 var want_jump := false
+var sprinting := false
+var sprint_timer := 0.0
 var home_position := Vector3.ZERO
 var destination := home_position
 func _physics_process(delta: float) -> void:
@@ -14,18 +17,22 @@ func _physics_process(delta: float) -> void:
 		destination = home_position + Vector3(WANDER_RANGE, 0, 0).rotated(Vector3.UP, randf_range(0, 2*PI))
 		parent.look_at(destination)
 	jump_timer = max(jump_timer - delta, 0.0)
+	sprint_timer = max(sprint_timer - delta, 0.0)
 	want_jump = false
 	if jump_timer > JUMP_RATE - 0.5:
 		want_jump = true
 	if jump_timer == 0.0:
 		jump_timer = JUMP_RATE + randf_range(-JUMP_RATE_SPREAD, JUMP_RATE_SPREAD)
+	if sprint_timer == 0.0:
+		sprint_timer = SPRINT_RATE
+		sprinting = not sprinting
 func move_input() -> Vector2:
 	if not parent:
 		return Vector2.ZERO
 	var direction := -parent.transform.basis.z
 	return Vector2(direction.x, direction.z)
 func sprint_input() -> bool:
-	return false
+	return sprinting
 func jump_input() -> bool:
 	return want_jump
 func _is_destination_reached() -> bool:
