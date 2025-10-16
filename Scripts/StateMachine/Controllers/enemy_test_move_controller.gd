b9cb3c077ef6f54376e63bf90eefb52d05b9e3b9
ROJS <mskim829@naver.com>
2025-10-16 22:18:13 +0900

    Create Plasma Projectile

--- Scripts/StateMachine/Controllers/enemy_test_move_controller.gd
+++ Scripts/StateMachine/Controllers/enemy_test_move_controller.gd
@@ -1,10 +1,12 @@
 class_name EnemyTestMoveController
 extends MoveController
-const WANDER_RANGE := 16.0
+const WANDER_RANGE := 12.0
 const JUMP_RATE := 3.5
 const JUMP_RATE_SPREAD := 1.0
 const SPRINT_RATE := 2.0
+const PAUSE_TIME := 3.0
 var jump_timer := 2.0
+var pause_timer := 0.0
 var want_jump := false
 var sprinting := false
 var sprint_timer := 0.0
@@ -16,8 +18,10 @@ func _physics_process(delta: float) -> void:
 	if _is_destination_reached():
 		destination = home_position + Vector3(WANDER_RANGE, 0, 0).rotated(Vector3.UP, randf_range(0, 2*PI))
 		parent.look_at(destination)
+		pause_timer = PAUSE_TIME
 	jump_timer = max(jump_timer - delta, 0.0)
 	sprint_timer = max(sprint_timer - delta, 0.0)
+	pause_timer = max(pause_timer - delta, 0.0)
 	want_jump = false
 	if jump_timer > JUMP_RATE - 0.5:
 		want_jump = true
@@ -28,6 +32,8 @@ func _physics_process(delta: float) -> void:
 		sprinting = not sprinting
 func move_input() -> Vector2:
 	if not parent:
+		return Vector2.ZERO
+	if pause_timer > 0.0:
 		return Vector2.ZERO
 	var direction := -parent.transform.basis.z
 	return Vector2(direction.x, direction.z)
