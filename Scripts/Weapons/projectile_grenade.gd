aa2c591c2ebb5ead211710b37c59b4ee969ddf8e
ROJS <mskim829@naver.com>
2025-10-17 14:43:58 +0900

    Implement Sticky grenade

--- Scripts/Weapons/projectile_grenade.gd
+++ Scripts/Weapons/projectile_grenade.gd
@@ -1,10 +1,7 @@
 extends Area3D
-const HITPARTICLE := preload("uid://pm7lgpgx10gl")
 const EXPLOSION := preload("uid://bp3mahismsqco")
-const GRAVITY := 9.8
+const GRAVITY := 6.0
 @export var lifetime_total := 1.0
-var base_damage := 2.5
-var damage_spread := 0.5
 var velocity := Vector3.ZERO
 var lifetime := lifetime_total
 var attached = false
@@ -17,18 +14,14 @@ func _physics_process(delta: float) -> void:
 	velocity.y -= GRAVITY*delta
 	position += velocity*delta
 func _on_area_entered(area: Node3D) -> void:
-	#check if the collider is an enemy
-	print("Collsiion")
-	var collider : Object = area.get_parent()
-	if collider.is_in_group("Enemy"):
-		var damage := base_damage
-		damage += randf_range(-1.0, 1.0)*damage_spread
-		collider.take_damage(damage)
-	var hit_particle : MeshInstance3D = HITPARTICLE.instantiate()
-	get_tree().current_scene.add_child(hit_particle)
-	hit_particle.global_position = global_position
-	hit_particle.set_direction(-velocity.normalized())
-	grenade_explode()
+	var collider : Node3D = area.get_parent()
+	if not collider.is_in_group("Enemy"):
+		return
+	set_attached()
+	var original_position = global_position
+	get_parent().remove_child(self)
+	collider.add_child(self)
+	global_position = original_position
 func grenade_explode() -> void:
 	var explosion : Area3D = EXPLOSION.instantiate()
 	get_tree().current_scene.add_child(explosion)
@@ -40,7 +33,8 @@ func grenade_explode() -> void:
 func set_lifetime(life : float)->void:
 	lifetime_total = life
 	lifetime = life
-
-
 func _on_body_entered(body: Node3D) -> void:
+	set_attached()
+func set_attached()->void:
 	attached = true
+	set_lifetime(0.4)
