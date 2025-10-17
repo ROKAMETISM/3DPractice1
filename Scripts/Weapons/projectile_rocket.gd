917741c7b97f1d9768b7bbdc50d28ca4dda50b13
ROJS <mskim829@naver.com>
2025-10-17 22:20:07 +0900

    Fix RocketLauncher

--- Scripts/Weapons/projectile_rocket.gd
+++ Scripts/Weapons/projectile_rocket.gd
@@ -2,24 +2,25 @@ extends Area3D
 const EXPLOSION := preload("uid://bp3mahismsqco")
 @export var lifetime_total := 1.0
 var velocity := Vector3.ZERO
-var lifetime := lifetime_total
+var _damage := 2.5
+var _lifetime := 1.0
 func _physics_process(delta: float) -> void:
-	lifetime -= delta
-	if lifetime <= 0.0:
+	_lifetime -= delta
+	if _lifetime <= 0.0:
 		rocket_explode()
 	position += velocity*delta
 func _on_area_entered(area: Node3D) -> void:
+	rocket_explode()
+func _on_body_entered(body: Node3D) -> void:
 	rocket_explode()
 func rocket_explode() -> void:
 	var explosion : Area3D = EXPLOSION.instantiate()
 	get_tree().current_scene.add_child(explosion)
 	explosion.global_position = global_position
 	explosion.affect_enemy(true)
-	explosion.set_damage(12.0)
+	explosion.set_damage(_damage)
 	explosion.explode()
 	queue_free()
-func set_lifetime(life : float)->void:
-	lifetime_total = life
-	lifetime = life
-func _on_body_entered(body: Node3D) -> void:
-	rocket_explode()
+func init(lifetime : float, damage:float)->void:
+	_lifetime = lifetime
+	_damage = damage
--- Scripts/Weapons/weapon_rocket_launcher.gd
+++ Scripts/Weapons/weapon_rocket_launcher.gd
@@ -8,10 +8,7 @@ func _fire_single_rocket() -> bool:
 	if _main_fire_timer > 0.0:
 		return false
 	_main_fire_timer = main_fire_rate
-	var aim_vector : Vector3 = _get_aim_with_spread(_adjusted_rotation, _spread_angle)
-	var projectile : Area3D = ROCKET.instantiate()
-	get_tree().current_scene.add_child(projectile)
-	projectile.global_position = global_position 
-	projectile.velocity = aim_vector.normalized()*projectile_speed
-	projectile.set_lifetime(15.0)
+	var aim_vector : Vector3 = _get_aim_with_spread(_adjusted_rotation, _spread_angle).normalized()
+	var projectile_damage := base_damage+randf_range(-1.0,1.0)*damage_spread
+	var projectile : Area3D = fire_projectile(ROCKET, projectile_damage, aim_vector, 15.0)
 	return true
