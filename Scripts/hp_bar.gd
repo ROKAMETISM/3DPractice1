9b8604115bd37c837692d1617ffbb29863747051
ROJS <mskim829@naver.com>
2025-10-18 12:28:25 +0900

    Update hp_bar.gd

--- Scripts/hp_bar.gd
+++ Scripts/hp_bar.gd
@@ -1,14 +1,5 @@
 extends ProgressBar
 @onready var hud := %HUD
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 func update()->void:
 	max_value = hud.player_hp_component.get_max_hp()
 	value = hud.player_hp_component.get_current_hp()
