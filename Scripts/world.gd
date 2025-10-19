43566747c009c43ad9515f657ba256af515c6e7b
ROJS <mskim829@naver.com>
2025-10-19 17:05:47 +0900

    Doc comments

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,10 +1,17 @@
+##GDScript for World < Node3D.
 extends Node3D
+##Const holding EnemyTest : PackedScene resource
 const ENEMY_TEST := preload("uid://du3tuubqmh3i8")
+##Const holding EneymCacodemon : PackedScene resource
 const ENEMY_CACODEMON := preload("uid://dl5ombgjk23so")
+##Const holding HPPotion : PackedScene resource
 const HPPotion := preload("uid://bgpam4vncl7bb")
+##reference to child node : Player
 @onready var player := %Player
+##reference to child node : HUD
 @onready var hud := %HUD
 func _ready() -> void:
+	#connect signals associated with HUD
 	player.position_updated.connect(hud._on_player_position_updated)
 	player.velocity_updated.connect(hud._on_player_velocity_updated)
 	player.y_acceleration_updated.connect(hud._on_player_y_acceleration_updated)
@@ -13,6 +20,7 @@ func _ready() -> void:
 	player.weapon_manager.ammo_updated.connect(hud._on_ammo_updated)
 	player.fsm.state_updated.connect(hud._on_player_fsm_state_updated)
 	player.entity_component.hp_component.hp_updated.connect(hud._on_player_hp_updated)
+	#init current and max ammo for HUD
 	for ammo_type in Weapon.AmmoType.values():
 		player.weapon_manager.ammo_updated.emit(ammo_type, player.weapon_manager.current_ammo[ammo_type])
 		hud.set_max_ammo(ammo_type, player.weapon_manager.max_ammo[ammo_type])
