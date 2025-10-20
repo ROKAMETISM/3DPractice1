cda7d842a24b7b6e0af0acd04b5575ccffa82ec3
ROJS <mskim829@naver.com>
2025-10-20 22:29:26 +0900

    Refactor ProjectileRocket

--- Scripts/Weapons/Projectiles/projectile_rocket.gd
+++ Scripts/Weapons/Projectiles/projectile_rocket.gd
@@ -1,19 +1,6 @@
-extends Area3D
+extends Projectile
 const EXPLOSION := preload("uid://bp3mahismsqco")
-@export var lifetime_total := 1.0
-var velocity := Vector3.ZERO
-var _damage := 2.5
-var _lifetime := 1.0
-func _physics_process(delta: float) -> void:
-	_lifetime -= delta
-	if _lifetime <= 0.0:
-		rocket_explode()
-	position += velocity*delta
-func _on_area_entered(area: Node3D) -> void:
-	rocket_explode()
-func _on_body_entered(body: Node3D) -> void:
-	rocket_explode()
-func rocket_explode() -> void:
+func _on_hit(collider:Node3D) -> void:
 	var explosion : Area3D = EXPLOSION.instantiate()
 	get_tree().current_scene.add_child(explosion)
 	explosion.global_position = global_position
@@ -21,6 +8,5 @@ func rocket_explode() -> void:
 	explosion.set_damage(_damage)
 	explosion.explode()
 	queue_free()
-func init(lifetime : float, damage:float)->void:
-	_lifetime = lifetime
-	_damage = damage
+func _on_lifetime_expired()->void:
+	_on_hit(null)
