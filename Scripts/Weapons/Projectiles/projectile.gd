cd76dd0dc3d0a9233492bba919f6ad729f474bf1
ROJS <mskim829@naver.com>
2025-10-20 19:35:30 +0900

    Add Raycast to projectile's behind so that it can detect when it clips through environment

--- Scripts/Weapons/Projectiles/projectile.gd
+++ Scripts/Weapons/Projectiles/projectile.gd
@@ -1,15 +1,19 @@
 class_name Projectile extends Area3D
 @export var hit_enemy := true
 @export var hit_player := false
+@export var direct_hit := true
 var _velocity := Vector3.ZERO
 var _damage := 2.5
 var _lifetime := 1.00
 var _source:Node3D=null
+@onready var raycast : RayCast3D = $RayCast3D
 func _ready() -> void:
 	collision_layer = 0b0
 	collision_mask = 0b0
+	raycast.collision_mask = 0b0
 	#collision mask 2 for hitting environment. Otherwise go through the environment.
 	set_collision_mask_value(2, true)
+	raycast.set_collision_mask_value(2, true)
 	if hit_enemy:
 		set_collision_mask_value(5, true)
 	if hit_player:
@@ -19,11 +23,16 @@ func _physics_process(delta: float) -> void:
 	if _lifetime <= 0.0:
 		queue_free()
 	position += _velocity * delta
+	raycast.target_position = -_velocity * delta
+	raycast.force_raycast_update()
+	if raycast.is_colliding():
+		global_position = raycast.get_collision_point()
+		_on_hit()
 func _on_area_entered(area: Node3D) -> void:
 	if not get_tree():
 		return
 	if area is EntityComponent:
-		if area.is_enemy:
+		if direct_hit and area.is_enemy:
 			area.take_hit(_source, _damage)
 	_on_hit()
 func _on_body_entered(_body: Node3D) -> void:
